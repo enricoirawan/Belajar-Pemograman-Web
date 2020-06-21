3fdb641ff7865b4bb5c64ae3606b1ee747ae21be
enricoirawan <enricoeusebius@gmail.com>
2020-06-21 22:48:18 +0700

    clean

--- toko_online/application/views/admin/dashboard.php
+++
@@ -1,3 +0,0 @@
-<div class="container-fluid">
-    <h4>Selamat Datang, silahkan cek penjualan hari ini.</h4>
-</div>
--- toko_online/application/views/proses_pesanan.php
+++
@@ -1,5 +0,0 @@
-<div class="container-fluid">
-    <div class="alert alert-success">
-        <p class="text-center align-center">Selamat Pesanan Anda Telah berhasil Di Proses.</p>
-    </div>
-</div>
